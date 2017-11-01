653ff0b9c8808ce3c1cfc90ab0d1cc9b2d73161f
1574001700@qq.com <1574001700@qq.com>
2017-11-01 19:37:28 +0800

    way

--- src/app/App.vue
+++ src/app/App.vue
@@ -1,29 +1,16 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <router-view/>
-=======
-  		
-  	<router-view/>
->>>>>>> 147fef6dc062474905e4d48781722b5f29013a96
   </div>
 </template>
 
 <script>
 export default {
-<<<<<<< HEAD
   name: 'app'
-=======
-  	name:'app'
->>>>>>> 147fef6dc062474905e4d48781722b5f29013a96
 }
 </script>
 
 <style>
-<<<<<<< HEAD
  @import "./reset.css";
  @import "../../node_modules/swiper/dist/css/swiper.css";
-=======
-	@import "./reset.css"
->>>>>>> 147fef6dc062474905e4d48781722b5f29013a96
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-<<<<<<< HEAD
 import Home from '@/pages/home/Home'
-=======
-import home from '@/pages/home/home.vue'
->>>>>>> 147fef6dc062474905e4d48781722b5f29013a96
 
 Vue.use(Router)
 
@@ -12,13 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-<<<<<<< HEAD
       name: 'Home',
       component: Home
-=======
-      name: 'home',
-      component: home
->>>>>>> 147fef6dc062474905e4d48781722b5f29013a96
     }
   ]
 })
